00e9414ab92076c0f89541d203a7cac093ac0ef2
13ARUN <13aruneswar@gmail.com>
2024-10-14 18:01:29 +0530

    logger initiated

--- todo/lib/services/database.dart
+++ todo/lib/services/database.dart
@@ -1,10 +1,13 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:todo/models/task_model.dart';
+import 'package:logger/logger.dart';
+
 
 class DataBase {
   static final DataBase _instance = DataBase._init();
   static Database? _database;
+  static final Logger _logger = Logger(); // Initialize logger
 
   static const String _tableName = 'tasks';
   static const String _columnId = 'id';
@@ -19,6 +22,7 @@ class DataBase {
 
   Future<Database> get database async {
     if (_database != null) return _database!;
+    _logger.i("Initializing database");
     _database = await _initializeDB('tasks.db');
     return _database!;
   }
@@ -27,6 +31,7 @@ class DataBase {
   Future<Database> _initializeDB(String filePath) async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
+    _logger.i("Database initialized at $path");
     return await openDatabase(
       path,
       version: 1,
@@ -36,6 +41,7 @@ class DataBase {
 
   //* Create DataBase
   Future _createDB(Database db, int version) async {
+    _logger.i("Creating table $_tableName");
     await db.execute('''
       CREATE TABLE $_tableName(
         $_columnId TEXT PRIMARY KEY,
@@ -45,50 +51,66 @@ class DataBase {
         $_columnIsCompleted INTEGER
       )
     ''');
+    _logger.i("Table $_tableName created");
   }
 
   //* Get All Tasks
   Future<List<TaskModel>> getTasks({String orderBy = _columnId}) async {
     final db = await database;
+    _logger.i("Fetching all tasks ordered by $orderBy");
     final result = await db.query(_tableName, orderBy: '$orderBy ASC');
+    _logger.i("Fetched ${result.length} tasks");
     return result.map((map) => TaskModel.fromMap(map)).toList();
   }
-
-  
 
   //* Add Task to DB
   Future<void> addTask(TaskModel task) async {
     final db = await database;
+    _logger.i("Inserting task with ID ${task.id}");
     await db.insert(_tableName, task.toMap());
+    _logger.i("Task with ID ${task.id} inserted");
   }
 
   //* Delete Task from DB
   Future<void> deleteTask(String id) async {
     final db = await database;
+    _logger.i("Deleting task with ID $id");
     await db.delete(_tableName, where: '$_columnId = ?', whereArgs: [id]);
+    _logger.i("Task with ID $id deleted");
   }
 
   //* Update Task in DB
   Future<void> updateTask(TaskModel task) async {
     final db = await database;
-    await db.update(_tableName, task.toMap(),
-        where: '$_columnId = ?', whereArgs: [task.id]);
+    _logger.i("Updating task with ID ${task.id}");
+    await db.update(
+      _tableName,
+      task.toMap(),
+      where: '$_columnId = ?',
+      whereArgs: [task.id],
+    );
+    _logger.i("Task with ID ${task.id} updated");
   }
 
   //* Delete Tasks
   Future<void> deleteTasks({bool completed = false}) async {
     final db = await database;
     if (completed) {
-      await db
-          .delete(_tableName, where: '$_columnIsCompleted = ?', whereArgs: [1]);
+      _logger.i("Deleting completed tasks");
+      await db.delete(_tableName, where: '$_columnIsCompleted = ?', whereArgs: [1]);
+      _logger.i("Completed tasks deleted");
     } else {
+      _logger.i("Deleting all tasks");
       await db.delete(_tableName);
+      _logger.i("All tasks deleted");
     }
   }
 
   //* Close DataBase
   Future<void> close() async {
     final db = await database;
+    _logger.i("Closing database");
     await db.close();
+    _logger.i("Database closed");
   }
 }
